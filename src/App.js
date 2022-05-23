189d4d801d89ded29b9c6802e1238fc043afac5d
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-05-23 11:18:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import "./event.css";
+import Register from './Register';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import UserLogin from './UserLogin';
+import AdminDashboard from './AdminComponents/AdminDashboard';
+import UserDashboard from './UserComponents/UserDashboard';
+import AdminLogin from './AdminComponents/AdminLogin';
+import EditEvent from './AdminComponents/EditEvent';
+import AddEvent from './AdminComponents/AddEvent';
+import ParticipateList from './AdminComponents/ParticipateList';
+import Registerform from './UserComponents/Registerform';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLogin />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/register" element={<Register />} />
+          {/* Admin Routes */}
+
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route path="/add-event" element={<AddEvent />} />
+          <Route path="/ParticipateList" element={<ParticipateList />} />
+          <Route path="/edit-event/:id" element={<EditEvent />} />
+
+          {/* User Routes */}
+         <Route path="/userdashboard" element={<UserDashboard />} />
+         <Route path="/registerform" element={<Registerform />} />
+
+    
+
+        </Routes>
+      </BrowserRouter>
+
+     
+
+    </>
   );
 }
 
-export default App;
+export default App; 
